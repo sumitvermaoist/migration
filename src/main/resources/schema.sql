8c07ff29b252f21fef62be7d5f388f77d86c6e86
Sumit Verma <sumitverma.oist@gmail.com>
2025-08-21 07:29:21 +0530

    data for table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -19,3 +19,15 @@ account_no    VARCHAR(50),
 api_status    CHAR(1),
 updated_by    VARCHAR(100),updated_time  TIMESTAMP
 );
+
+INSERT INTO hram_fulcom (account_no, api_status, updated_by, updated_time)
+VALUES ('ACC1001', 'S', 'system', CURRENT_TIMESTAMP);
+
+INSERT INTO hram_fulcom (account_no, api_status, updated_by, updated_time)
+VALUES ('ACC1002', 'F', 'admin', TIMESTAMP '2025-08-21 10:15:00');
+
+INSERT INTO hram_fulcom (account_no, api_status, updated_by, updated_time)
+VALUES ('ACC1003', 'S', 'batch_job', TIMESTAMP '2025-08-20 18:30:00');
+
+INSERT INTO hram_fulcom (account_no, api_status, updated_by, updated_time)
+VALUES ('ACC1004', 'P', 'user1', TIMESTAMP '2025-08-19 09:00:00');
